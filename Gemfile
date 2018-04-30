6115bf1ce7112cba49687e1cc9ef600f8e550ebd
kenjikojima <k.kojima09211986@gmail.com>
2018-04-30 10:08:27 +0000

    add contact page

--- Gemfile
+++ Gemfile
@@ -33,6 +33,5 @@ group :production do
   gem 'pg', '0.18.4'
 end
 
-
 # Windows環境ではtzinfo-dataというgemを含める必要があります
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,5 +23,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+   end 
+    
+   test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
+    
+  
 end
