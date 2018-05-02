705768e9b576ef4f15b65df5e69a9cb208d92fea
kenjikojima <k.kojima09211986@gmail.com>
2018-05-02 15:08:46 +0000

    5.1.2 complete

---
+++ example_user.rb
@@ -0,0 +1,29 @@
+class User
+  attr_accessor :first_name, :last_name, :email
+
+
+  def initialize(attributes = {})
+    @first_name  = attributes[:first_name]
+    @last_name   = attributes[:last_name]
+    @email       = attributes[:email]
+  end
+
+  def full_name
+    @full_name = @first_name + @last_name
+  end
+  
+  def alphabetical_name
+    @alphabetical_name = "#{@first_name}, #{@last_name}"
+  end
+
+ def formatted_email_full
+  "#{@full_name} <#{@email}>"
+ end
+
+ def formatted_email_al
+  "#{@alphabetical_name} <#{@email}>"
+ end
+
+
+end
+
