c33156370836805ac34941d8b2337aaad1d428d3
kenjikojima <k.kojima09211986@gmail.com>
2018-05-11 15:14:41 +0000

    Chapter10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,26 @@
 class UsersController < ApplicationController
- 
+  before_action :logged_in_user, only: [:index, :edit, 
+                                       :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   # Get /users/:id
   def show
     #=> app/views/users/show.html
     @user = User.find(params[:id])
   end
   
+  # GET /users/new
   def new
    @user = User.new
+   # => form_for @user
   end
   
+  # Post /users
   def create
     @user = User.new(user_params)
     if @user.save
@@ -24,9 +35,65 @@ class UsersController < ApplicationController
     end
   end  
   
-  def user_params
-    params.require(:user).permit(
-      :name, :email, :password, 
-      :password_confirmation)
+  #GET /users/:id/edit
+  #params[:id] => :id
+  def edit
+    @user = User.find(params[:id])
+    # app/views/users/edit.html.erb
   end
+  
+  
+  # PATCH /users/:id
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #Success
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      #Failure
+      # => @user.errors.full_messages
+      render 'edit'
+    end
+  end
+  
+  # DELETE /users/:id
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(
+        :name, :email, :password, 
+        :password_confirmation)
+    end
+    
+    # ログイン済みユーザーかどうか確認
+      def logged_in_user
+        unless logged_in?
+         #GET   /users/:id/edit 
+         #PATCH /users/:
+         # => GET /users/:id
+          store_location
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
+      end
+  
+  # 正しいユーザーかどうか確認
+    def correct_user
+      #GET   /users/:id/edit 
+      #PATCH /users/:id
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  
+  # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,10 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
   
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
@@ -46,6 +50,17 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+   # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
   
   
