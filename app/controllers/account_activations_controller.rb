3fe296a5a5bc1d954adc3958b6fbd49692be3150
kenjikojima <k.kojima09211986@gmail.com>
2018-05-18 15:58:36 +0000

    1

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,10 +5,10 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate     
       log_in user
-      flash[:success] = "Account activated!"
+      flash[:success] = "アカウントは有効化されました！"
       redirect_to user
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = "アカウントは無効化です"
       redirect_to root_url
     end
   end
