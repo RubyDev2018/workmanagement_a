3be5a57f71fc815ad57e255624af15d1ae6d0a7a
kenjikojima <k.kojima09211986@gmail.com>
2018-05-19 15:00:55 +0000

    A

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title,         "RUN CLUB"
+    assert_equal full_title("Help"), "Help | RUN CLUB"
   end
 end
