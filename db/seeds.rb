fdbfb0a3c7b67027317de0907591e5aae7b52778
kenji0921 <k.kojima09211986@gmail.com>
2018-06-25 13:52:50 +0000

    modify

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.create!(name:  "Example User",
 99.times do |n|
   name     = Faker::Name.name
   email    = "example-#{n+1}@railstutorial.org"
-  affiliation = "ユーザー",
+  affiliation = "ユーザー"
   password = "password"
   User.create!(name:  name,
                email: email,
